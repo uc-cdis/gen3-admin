0c5a49c5ad46de038f1496d6fde9dd9b1c6c7d02
Jawad Qureshi <qureshi@uchicago.edu>
2024-03-06 11:45:28 -0600

    Exec in python

--- gen3-admin-frontend/components/DeploymentDetails/DeploymentDetails.tsx
+++ gen3-admin-frontend/components/DeploymentDetails/DeploymentDetails.tsx
@@ -8,12 +8,13 @@ import { Title, Text, Anchor, Badge, Progress, Button, Table, HoverCard, Tooltip
 import { useEffect, useState } from 'react';
 import { differenceInMinutes, differenceInHours, differenceInDays, format } from 'date-fns';
 
-  
+
 
 async function fetchDeployment(name) {
-  console.log(name)
+
+    console.log(name)
     try {
-        const response = await fetch("/admin-api-go/deployments/"+name); // This endpoint will be redirected by your proxy
+        const response = await fetch("/admin-api-go/deployments/" + name); // This endpoint will be redirected by your proxy
         if (!response.ok) {
             throw new Error(`Error: ${response.status}`);
         }
@@ -26,42 +27,40 @@ async function fetchDeployment(name) {
     }
 }
 
-export function DeploymentDetails({name}) {
+export function DeploymentDetails({ name }) {
     const [deployments, setDeployments] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
     console.log(name)
 
     useEffect(() => {
+        if (!name) {
+            return;
+        }
         setIsLoading(true);
         fetchDeployment(name).then((data) => {
             console.log(data);
-            // check if data pods exists
-            if(data){
-              setDeployments(data?.pods);
-            }
+            setDeployments(data?.pods);
             setIsLoading(false);
         });
     }, []);
 
-    const Body = deployments.map((deployment, index) => 
-    
-            {
-                return(
-                <Table.Tr key={index}>
-                    <Table.Td>
-                        {deployment.ready ? <Badge 
-                                variant="gradient"
-                                gradient={{ from: 'green', to: 'lime', deg: 90 }}>Active</Badge> 
-                        : <Badge 
-                                variant="gradient"
-                                gradient={{ from: 'red', to: 'orange', deg: 90 }}>Inactive</Badge>}
-                    </Table.Td>
-                    <Table.Td>
-                        {/* <HoverCard width={100}>
+    const Body = deployments.map((deployment, index) => {
+        return (
+            <Table.Tr key={index}>
+                <Table.Td>
+                    {deployment.ready ? <Badge
+                        variant="gradient"
+                        gradient={{ from: 'green', to: 'lime', deg: 90 }}>Active</Badge>
+                        : <Badge
+                            variant="gradient"
+                            gradient={{ from: 'red', to: 'orange', deg: 90 }}>Inactive</Badge>}
+                </Table.Td>
+                <Table.Td>
+                    {/* <HoverCard width={100}>
                             <HoverCard.Target> */}
-                                <Anchor>{deployment.name}</Anchor>
-                            {/* </HoverCard.Target>
+                    <Anchor>{deployment.name}</Anchor>
+                    {/* </HoverCard.Target>
                             <HoverCard.Dropdown>
                                 {
                                     // Print labels as string
@@ -71,29 +70,30 @@ export function DeploymentDetails({name}) {
                                 }
                             </HoverCard.Dropdown>
                         </HoverCard> */}
-                    </Table.Td>
-                    <Table.Td>{deployment.image}</Table.Td>
-                    <Table.Td>{deployment.ready}/{deployment.desired}</Table.Td>
-                    <Table.Td>1</Table.Td>
-                    <Table.Td>1</Table.Td>
-                    <Table.Td>21</Table.Td>
-                    <Table.Td>
+                </Table.Td>
+                <Table.Td>{deployment.image}</Table.Td>
+                <Table.Td>{deployment.ready}/{deployment.desired}</Table.Td>
+                <Table.Td>1</Table.Td>
+                <Table.Td>1</Table.Td>
+                <Table.Td>21</Table.Td>
+                <Table.Td>
 
-                        {deployment.ready ? <Progress color="green" value={100}> </Progress> : deployment.readyReplicas == 0 ? <Progress color="red" value={100}> </Progress>: <Progress color="yellow" value={deployment.ready/deployment.desired}> </Progress>}                       
+                    {deployment.ready ? <Progress color="green" value={100}> </Progress> : deployment.readyReplicas == 0 ? <Progress color="red" value={100}> </Progress> : <Progress color="yellow" value={deployment.ready / deployment.desired}> </Progress>}
 
-                    </Table.Td>
-                    <Table.Td><Button>..</Button> </Table.Td>
-                </Table.Tr>
-        )});
+                </Table.Td>
+                <Table.Td><Button>..</Button> </Table.Td>
+            </Table.Tr>
+        )
+    });
 
     return (
         <>
-        <Title ta="center" my={20}>
+            <Title ta="center" my={20}>
                 This is the {' '}
                 <Text inherit variant="gradient" component="span" gradient={{ from: 'blue', to: 'black' }}>
                     Deployments
-                    </Text>
-                    {' '} Overview Page
+                </Text>
+                {' '} Overview Page
             </Title>
 
             {deployments.length > 0 ? (
